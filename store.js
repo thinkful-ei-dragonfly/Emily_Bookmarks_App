fbf4212af44cbc779d0d2f91e8d64536b7158087
ebrocato <emilybrocato@outlook.com>
2019-05-10 10:51:33 -0600

    added toggle feature on form, added visit button, fixed title toggle error

--- store.js
+++ store.js
@@ -3,7 +3,7 @@
 
 let ID = 0;
 const STORE = [
- {
+  {
     id: 0, title: 'Article on Cats', url: 'http://www.catsrcool.com',
     description: 'article about cats', rating: 5, expanded: true
   },
@@ -14,48 +14,51 @@ const STORE = [
 
 ];
 
+function bookmarkToHtml(bookmark) {
+  let bookmarkHTML =
+    `<li class="click-to-hide">
+            <h3>${bookmark.title}</h3>
+            <p class="hide-this">${bookmark.description}</p>
+            <button class="hide-this">${bookmark.url}</button>
+            <p>${bookmark.rating}</p>
+      </li>`;
+  return bookmarkHTML;
+
+}
+
+
+
+
+
+
 function displayResults(minRating) {
+  //step 1: empty existing list 
   $('#new-bookmark-list').empty();
-
+  //step 2: loop through items in store
   for (let i = 0; i < STORE.length; i++) {
-    
-    let  bookmark = STORE[i];
-    console.log(typeof(minRating) );
-    console.log( typeof(bookmark) );
-    if(minRating == 'all') {
-      console.log(bookmark);
-      let bookmarkHTML = 
-        `<li class="click-to-hide">
-            <h3>${bookmark.title}</h3>
-            <p class="hide-this">${bookmark.description}</p>
-            <p>${bookmark.rating}</p>
-        </li>`;
-
-      console.log(bookmarkHTML);
+    let bookmark = STORE[i];
+    if (minRating == 'all') {
+      let bookmarkHtml = bookmarkToHtml(bookmark);
       let bookmarkListElement = $('#new-bookmark-list');
-      bookmarkListElement.append(bookmarkHTML);
-      
+      bookmarkListElement.append(bookmarkHtml);
+      hideDescription();
 
-    } else if (minRating == bookmark.rating){  
-      let bookmarkHTML =
-        `<li class="click-to-hide">
-            <h3>${bookmark.title}</h3>
-            <p class="hide-this">${bookmark.description}</p>
-            <p>${bookmark.rating}</p>
-        </li>`;
+    } else if (minRating == bookmark.rating) {
+      let bookmarkHTML = bookmarkToHtml(bookmark);
 
-      console.log(bookmarkHTML);
       let bookmarkListElement = $('#new-bookmark-list');
       bookmarkListElement.append(bookmarkHTML);
       hideDescription();
-    
+
     }
   }
 
-  
+}
 
-} //  $('#new-bookmark-list').append(bookmarkHTML);
-  
+
+
+
+
 
 
 
@@ -64,7 +67,7 @@ function displayResults(minRating) {
 $("#filter-bookmark-rating").on("change", handleRatingChange)
 //need to review this
 
-function handleRatingChange(event){
+function handleRatingChange(event) {
   let rating = $('#filter-bookmark-rating').val();//event.target.val()
 
   displayResults(rating);
@@ -81,26 +84,31 @@ function handleRatingChange(event){
 // }
 
 function hideDescription() {
-  $('.click-to-hide').click(function () {
-    $(this).children('.hide-this').toggle();
-    
-  
-    
-    
-    
-  });
+  $(document).ready(function () {
+    $('.click-to-hide').click(function () {
+      $(this).children('.hide-this').toggle();
 
-  
+    });
+  });
+}
+
+function hideForm() {
+  $(document).ready(function () {
+    $('#click-to-toggle-form').click(function (event) {
+      event.preventDefault();
+      $('.hide-form').toggleClass('hidden');
+    });
+
+  });
 }
 
 
 
-
 function handleSubmitNewBookmark() {
-  $('#add-new-bookmark').on("submit", function(event) {
-      event.preventDefault();
-      console.log(event.target);
-      
+  $('#add-new-bookmark').on("submit", function (event) {
+    event.preventDefault();
+    console.log(event.target);
+
     ID += 1
     const newBookmark = {
       id: ID,
@@ -108,20 +116,20 @@ function handleSubmitNewBookmark() {
       url: $('#bookmark-url').val(),
       description: $('#bookmark-description').val(),
       rating: $('#bookmark-rating').val(),
-      expanded: false, 
+      expanded: false,
     }
-  console.log(newBookmark);
+    console.log(newBookmark);
     STORE.push(newBookmark);
     displayResults("all");
-   
-   
+
+
   });
 }
 
 handleSubmitNewBookmark();
-
-
 displayResults("all");
 hideDescription();
+hideForm();
+
 
 
